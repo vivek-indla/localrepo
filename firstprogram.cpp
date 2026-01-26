4dcebbd75fa88d736428d9a5325c505857cfa13f
vivek indla <25215a7213@bvrit.ac.in>
2026-01-26 15:02:34 +0530

    changed

---
+++ firstprogram.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello,programmer "<<endl;
+    cout<<"let's start the journey of our programming "<endl;
+    cout<<"complete the tasks and make something that never happened in the history"<<endl;
+}   
