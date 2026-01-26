409b4c02ceeaa23f987faf18b4c9626b35138cd4
vivek indla <25215a7213@bvrit.ac.in>
2026-01-26 15:13:06 +0530

    commited

--- firstprogram.cpp
+++ firstprogram.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main(){
     cout<<"Hello,programmer "<<endl;
-    cout<<"let's start the journey of our programming "<endl;
+    cout<<"let's start the journey of our programming "<<endl;
     cout<<"complete the tasks and make something that never happened in the history"<<endl;
 }   
